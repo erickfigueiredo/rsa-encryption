3acf15bfd75151b233ae4a678ed548981d8a238d
Sávio Miranda <savio.miranda@dtidigital.com.br>
2021-10-23 16:29:29 -0300

    Added sign and sign varification

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include<string>
 
 #include "KeysGenerator.h"
 #include "ModularPower.h"
@@ -12,15 +13,17 @@ using namespace std;
 void encoding();
 void decoding();
 void sign();
+void validateSign();
 
 int main() {
      //Inicializa a semente da função aleatória
     srand(time(0));
 
-    cout << "Olá, escolha a operação desejada:\n\n";
-    cout << "1) Codificação\n";
-    cout << "2) Decodificação\n";
-    cout << "3) Assinatura\n\n";
+    cout << "Ola, escolha a operacao desejada:\n\n";
+    cout << "1) Codificacao\n";
+    cout << "2) Decodificacao\n";
+    cout << "3) Assinar mensagem\n\n";
+    cout << "4) Verificar assinatura\n\n";
 
     int option;
     cin >> option;
@@ -34,6 +37,9 @@ int main() {
             break;
         case 3:
             sign();
+            break;
+        case 4:
+            validateSign();
             break;
     }
 
@@ -68,7 +74,7 @@ void encoding(){
 
     // Apenas demonstrativo
     cout << "Mensagem criptografada: ";
-    for(const long long num: msgInRSA) cout << num;
+    for(const long long num: msgInRSA) cout << num << " ";
     cout << endl;
 
     // Mensagem é descriptografada 
@@ -86,15 +92,15 @@ void decoding(){
     vector<int> msgReversed;
 
     string entry;
-    cout << "Informe a mensagem criptografada: ";
-    while(cin >> entry){
+    cout << "Informe a mensagem criptografada separando as cifras por espaço, para finalizar a entrada digite -1: ";
+    while(cin >> entry && entry != "-1"){
         msg.push_back(stoi(entry));
     }
 
     cout << "Informe a chave privada: ";
     cin >> d;
 
-    cout << "informe o N público: ";
+    cout << "informe o N: ";
     cin >> n;
 
     for (auto num : msg) {
@@ -103,4 +109,57 @@ void decoding(){
 
     // Decodames a mensagem
     cout << "Mensagem descriptografada: " << decode(msgReversed) << endl; 
+}
+
+void sign(){
+    string entry, aux;
+
+    long long n, d;
+    vector<long long> msgInRSA;
+    vector<int> msgReversed;
+
+    cout << "Informe a mensagem: ";
+    cin >> entry;
+
+    cout << "Informe a chave privada para assinar: ";
+    cin >> d;
+
+    cout << "Informe o N para assinar: ";
+    cin >> n;
+
+    string enc = encode(entry);
+    cout << enc << endl;
+
+    // Mensagem enc é assinada
+    long long signature = modPow(stoi(enc), d, n);
+
+    cout << "Assinatura: " << signature << endl;
+    
+}
+
+void validateSign(){
+    string entry, sign;
+
+    long long n, e, d;
+
+    cout << "Informe a mensagem esperada: ";
+    cin >> entry;
+
+    cout << "Informe a mensagem assinada: ";
+    cin >> sign;
+
+    cout << "Informe o e para verificar a procedencia: ";
+    cin >> e;
+    cout << "Informe o n para verificar a procedencia: ";
+    cin >> n;
+
+    string hash = encode(entry);
+    cout << "hash " << hash << endl;
+    // Mensagem entry é assinada
+    long long hashSign = modPow(stoi(sign), e, n);
+    //string r = decode()
+
+    cout << "hashSign " << hashSign << endl;
+
+    cout << "Assinatura valida: " << (hash == to_string(hashSign)) ? "Sim!" : "Nao";
 }
